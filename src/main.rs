8a43283fd6738314d4c66d473e4caf1ede8894db
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-03-26 17:59:50 +0300

    Use typst --watch to recompile files

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use eyre::Result;
 use state::ServerState;
 use std::{env, fs, sync::Arc};
 use tokio::{runtime::Runtime, signal::ctrl_c, sync::Notify};
-use tracing::{info, warn};
-use watcher::compile_typst;
+use tracing::{error, info, warn};
 
 mod routes;
 mod state;
@@ -18,7 +17,7 @@ async fn run(state: Arc<ServerState>) -> Result<()> {
         .with_state(state.clone());
 
     let server = Server::bind(&"0.0.0.0:5599".parse()?).serve(router.into_make_service());
-    println!("Server is listening on http://{}/", server.local_addr());
+    info!("Server is listening on http://{}/", server.local_addr());
 
     tokio::select! {
         _ = server => {},
@@ -35,12 +34,12 @@ fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let Some(file) = env::args().nth(1) else {
-        println!("Usage: ./typst-live <file.typ>");
+        info!("Usage: ./typst-live <file.typ>");
         return Ok(());
     };
 
     if fs::metadata(&file).is_err() {
-        println!("File `{file}` doesn't exist");
+        error!("File `{file}` doesn't exist");
         return Ok(());
     }
 
@@ -59,10 +58,9 @@ fn main() -> Result<()> {
 
     state.tokio.spawn(graceful_shutdown(state.clone()));
 
-    // Compile first time to create `output.pdf`
-    state.tokio.block_on(compile_typst(file, state.clone()));
-
-    let _watcher = watcher::setup_watch(state.clone())?;
+    let _watcher = state
+        .tokio
+        .block_on(watcher::setup_watching_typst(state.clone()))?;
     state.tokio.block_on(run(state.clone()))?;
 
     Ok(())
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,51 +1,51 @@
 use crate::state::ServerState;
 use eyre::Result;
 use notify::{Event, EventKind, INotifyWatcher, RecursiveMode, Watcher};
-use std::{path::Path, sync::Arc, time::Duration};
+use std::{path::Path, process::Stdio, sync::Arc, time::Duration};
 use tokio::{process::Command, time::Instant};
 use tracing::{debug, error};
 
-pub fn setup_watch(state: Arc<ServerState>) -> Result<INotifyWatcher> {
+pub async fn setup_watching_typst(state: Arc<ServerState>) -> Result<INotifyWatcher> {
     let mut last_update = Instant::now();
+    let typstname = state.typstname.read().await.clone();
+
+    match Command::new("typst")
+        .arg("--watch")
+        .arg(&typstname)
+        .arg("output.pdf")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+    {
+        Ok(child) => {
+            _ = state.tokio.spawn(async move {
+                match child.wait_with_output().await {
+                    Ok(out) if !out.status.success() => {
+                        error!("Typst exited with error code: {}", out.status)
+                    }
+                    Err(err) => error!("Typst exited with error: {err:?}"),
+                    _ => {}
+                }
+            })
+        }
+        Err(err) => error!("Failed to spawn the typst {err:?}"),
+    }
 
     let mut watcher = notify::recommended_watcher(move |e: Result<Event, _>| match e {
         Ok(e) if matches!(e.kind, EventKind::Modify(_)) => {
             debug!("Received notify event");
 
-            let typstname = state.typstname.blocking_read().clone();
-
-            if e.paths.iter().any(|p| p.ends_with(&typstname))
+            if e.paths.iter().any(|p| p.ends_with("output.pdf"))
                 && last_update.elapsed() > Duration::from_millis(100)
             {
                 last_update = Instant::now();
-                debug!("{typstname} was updated, recompiling");
-
-                state.tokio.spawn(compile_typst(typstname, state.clone()));
+                state.changed.notify_waiters();
             }
         }
-        Err(err) => eprintln!("Error: {err}"),
+        Err(err) => error!("Error: {err}"),
         _ => {}
     })?;
     watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
 
     Ok(watcher)
-}
-
-pub async fn compile_typst(typstname: String, state: Arc<ServerState>) {
-    let result = Command::new("typst")
-        .arg(typstname)
-        .arg("output.pdf")
-        .output()
-        .await;
-
-    match result {
-        Ok(out) => {
-            if out.status.success() {
-                state.changed.notify_waiters();
-            } else {
-                error!("Typst exited with {} status code", out.status);
-            }
-        }
-        Err(err) => error!("Error running typst: {err:?}"),
-    }
 }
